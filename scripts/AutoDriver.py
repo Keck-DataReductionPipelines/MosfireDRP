9f5fa196ab39e4d72830767949923d5c14ef0cec
Tuan Do <tdo@astro.ucla.edu>
2015-07-14 13:44:43 -0700

    copying over new AutoDriver.py from apps to scripts directory

--- scripts/AutoDriver.py
+++ scripts/AutoDriver.py
@@ -171,6 +171,7 @@ class Driver:
             else:
                 self.waveName = "lambda_solution_"+str(Wavelength.filelist_to_wavename(files, self.band, self.maskName,""))            
         if self.type is 'long2pos' or self.type is 'long2pos_specphot':
+            calibWith = ""
             if self.isEmpty('Ar.txt') is False: 
                 self.addLine("argon = ['Ar.txt']")
                 calibWith = "argon"
@@ -179,8 +180,18 @@ class Driver:
                 self.addLine("neon = ['Ne.txt')")
                 calibWith = "neon"
                 waveFiles = IO.list_file_to_strings('Ne.txt')            
+            if not calibWith:
+                calibWith='obsfiles'
+                print "OFFSET FILES"+str(self.offsetFiles)
+                waveFiles = IO.list_file_to_strings(self.offsetFiles)
+                self.addLine("obsfiles = obsfiles_posAnarrow+obsfiles_posCnarrow")
             self.addLine("Wavelength.imcombine("+str(calibWith)+", maskname, band, waveops)")
-            self.addLine("Wavelength.fit_lambda_interactively(maskname, band, "+str(calibWith)+",waveops,longslit=longslit, "+str(calibWith)+"=True)")
+            LineToAdd = "Wavelength.fit_lambda_interactively(maskname, band, "+str(calibWith)+",waveops,longslit=longslit"
+            if calibWith != 'obsfiles':
+                LineToAdd = LineToAdd + ","+str(calibWith)+"=True)"
+            else:
+                LineToAdd = LineToAdd + ")"
+            self.addLine(LineToAdd)
             self.addLine("Wavelength.fit_lambda(maskname, band, "+str(calibWith)+","+str(calibWith)+",waveops,longslit=longslit)")
             self.addLine("Wavelength.apply_lambda_simple(maskname, band, "+str(calibWith)+", waveops, longslit=longslit, smooth=True)")            
             self.waveName = "lambda_solution_"+str(Wavelength.filelist_to_wavename(waveFiles, self.band, self.maskName,""))
@@ -188,8 +199,11 @@ class Driver:
         self.addLine("")
             
     def printBackground(self):
-        if self.type is 'long2pos_specphot' or self.type is 'long2pos':
+        if self.type is 'long2pos_specphot':
             for slit in ['posAnarrow','posCnarrow','posAwide','posCwide']:
+                self.addLine("Background.handle_background(obsfiles_"+str(slit)+",Wavelength_file,maskname,band,waveops, target=target_"+str(slit)+")")
+        if self.type is 'long2pos':
+            for slit in ['posAnarrow','posCnarrow']:
                 self.addLine("Background.handle_background(obsfiles_"+str(slit)+",Wavelength_file,maskname,band,waveops, target=target_"+str(slit)+")")
         if self.type is 'slitmask' or self.type is 'longslit':
             self.addLine("Background.handle_background(obsfiles,'"+str(self.waveName)+"',maskname,band,waveops)")
@@ -304,7 +318,10 @@ def SetupFiles(target=None, offsets=None, type=None):
         # narrow slits
         if set([-7,-21,7,21]).issubset(offsets):
             setupLines.append("obsfiles_posCnarrow = ['Offset_-21_"+str(target)+"_PosC.txt', 'Offset_-7_"+str(target)+"_PosC.txt']")
-            obsFiles.append("Offset_-21_"+str(target)+"_PosC.txt")  # we are using this to determine maskname and band
+            obsFiles.append("Offset_7_"+str(target)+"_PosA.txt")  # we are using this to determine maskname and bad
+            obsFiles.append("Offset_21_"+str(target)+"_PosA.txt")  
+            obsFiles.append("Offset_-21_"+str(target)+"_PosC.txt")  
+            obsFiles.append("Offset_-7_"+str(target)+"_PosC.txt")                          
             setupLines.append('target_posCnarrow = "'+str(target)+'_POSC_NARROW"')
             setupLines.append("IO.fix_long2pos_headers(obsfiles_posCnarrow)")
             setupLines.append("obsfiles_posAnarrow = ['Offset_7_"+str(target)+"_PosA.txt', 'Offset_21_"+str(target)+"_PosA.txt']")
@@ -323,7 +340,10 @@ def SetupFiles(target=None, offsets=None, type=None):
         # narrow slits
         if set([-7,7]).issubset(offsets) and not(set([21,21]).issubset(offsets)):
             setupLines.append("obsfiles_posCnarrow = ['Offset_7_"+str(target)+"_PosC.txt', 'Offset_-7_"+str(target)+"_PosC.txt']")
+            obsFiles.append("Offset_7_"+str(target)+"_PosA.txt")  # we are using this to determine maskname and band
+            obsFiles.append("Offset_-7_"+str(target)+"_PosA.txt")
             obsFiles.append("Offset_7_"+str(target)+"_PosC.txt")  # we are using this to determine maskname and band
+            obsFiles.append("Offset_-7_"+str(target)+"_PosC.txt")            
             setupLines.append('target_posCnarrow = "'+str(target)+'_POSC_NARROW"')
             setupLines.append("obsfiles_posAnarrow = ['Offset_7_"+str(target)+"_PosA.txt', 'Offset_-7_"+str(target)+"_PosA.txt']")
             setupLines.append('target_posAnarrow = "'+str(target)+'_POSA_NARROW"')
