5c92bfdecac2f7c7101f1be829f6ed55d2aa53ab
Luca Rizzi <lrizzi@keck.hawaii.edu>
2015-07-31 13:44:31 -1000

    fixed push mistake on handle.py

--- apps/handle.py
+++ apps/handle.py
@@ -64,11 +64,7 @@ for fname in files:
 
 
     datafile = header['datafile'] + '.fits'
-<<<<<<< HEAD
     maskname = str(header['maskname'])
-=======
-    maskname = header['maskname']
->>>>>>> 2015A
     target = str(header['targname'])
     filter = header['filter']
     yr,mn,dy = IO.fname_to_date_tuple(datafile)
