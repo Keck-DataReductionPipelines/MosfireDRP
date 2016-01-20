eff115152002a6c6436b591d600e006940c85983
Luca Rizzi <lrizzi@keck.hawaii.edu>
2016-01-19 14:56:59 -1000

    Stop autodriver if there are no offsets files to reduce

--- apps/AutoDriver.py
+++ apps/AutoDriver.py
@@ -261,6 +261,13 @@ class Driver:
 def OffsetPairs():
 
     offsetFiles = glob.glob("Offset_*.txt")
+    if len(offsetFiles)==0:
+        print "##############################################"
+        print "WARNING: There are no Offset files"
+        print "         Are you sure you observed this mask?"
+        print "##############################################"
+        sys.exit(0)
+        
     # remove Offset_ and remote .txt
     tmpOffsets = [off.replace("Offset_","").replace(".txt","") for off in offsetFiles]
     # for each name, separate using _ as a separator
