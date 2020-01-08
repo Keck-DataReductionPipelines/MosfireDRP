Keck-DataReductionPipelines/MosfireDRP

commit f35382ecb747e0afbfd606413149639876a55f3e

Flats: adding more code to handle lamps off flats in K band

MOSFIRE/Flats.py
@@ -138,6 +138,9 @@ def handle_flats(flatlist, maskname, band, options, extension=None,edgeThreshold
     debug("Combined '%s' to '%s'" % (flatlist, maskname))
 #     info("Combined flats for '%s'" % (maskname))
     path = "combflat_2d_%s.fits" % band
+    if lampsOffList != None:
+        debug("Using on-off flat for K band")
+        path = os.path.join("combflat_lamps_on_2d_{:s}.fits".format(band))
     (header, data) = IO.readfits(path, use_bpm=True)
     info("Flat written to %s" % path)
 

commit f3fd790a8f6796141ccf8a5e22717d3461963ff0

Fix lampOffList typo in Flats.py (introduced in: f35382e)

MOSFIRE/Flats.py
@@ -138,7 +138,7 @@ def handle_flats(flatlist, maskname, band, options, extension=None,edgeThreshold
     debug("Combined '%s' to '%s'" % (flatlist, maskname))
 #     info("Combined flats for '%s'" % (maskname))
     path = "combflat_2d_%s.fits" % band
-    if lampsOffList != None:
+    if lampOffList != None:
         debug("Using on-off flat for K band")
         path = os.path.join("combflat_lamps_on_2d_{:s}.fits".format(band))
     (header, data) = IO.readfits(path, use_bpm=True)